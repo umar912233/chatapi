2e7191a04b1d1c3629e46c17fbe6b740eaa5014f
umar912233 <60961611+umar912233@users.noreply.github.com>
2023-02-26 18:16:37 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -37,6 +37,31 @@ connection.connect(function(err) {
   // });
 });
 
+
+let io = require('socket.io')(server,
+  {
+    transports: ['websocket'],
+    secure: true,
+    cors: {
+      origin: "*"
+    }
+  });  
+io.on('connection', (socket) => {
+
+  socket.on('disconnect', function(){
+    io.emit('users-changed', {user: socket.username, event: 'left'});
+  });
+
+  socket.on('set-name', (name) => {
+    socket.username = name;
+    io.emit('users-changed', {user: name, event: 'joined'});
+  });
+
+  socket.on('send-message', (message) => {
+    io.emit('message', {msg: message.text, user: socket.username, createdAt: new Date()});
+  });
+});
+
 app.post('/login', function (req, res) {
 
   let username = req.body.email;
@@ -110,31 +135,6 @@ app.post('/login', function (req, res) {
   });
  
  });
-
-let io = require('socket.io')(server,
-  {
-    cors: {
-      origin: "*"
-    }});  
-io.on('connection', (socket) => {
-
-  socket.on('disconnect', function(){
-    io.emit('users-changed', {user: socket.username, event: 'left'});
-  });
-
-  socket.on('set-name', (name) => {
-    socket.username = name;
-    io.emit('users-changed', {user: name, event: 'joined'});
-  });
-
-  socket.on('send-message', (message) => {
-    io.emit('message', {msg: message.text, user: socket.username, createdAt: new Date()});
-  });
-});
-
-
-
-
 
 
 var port = process.env.PORT || 3001;
